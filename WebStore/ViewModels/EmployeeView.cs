7cf8f80f93e395fac070fde2fe1a52443ee855b1
fargo-mm <52330752+fargo-mm@users.noreply.github.com>
2019-06-30 22:09:51 +0300

    Add files via upload

---
+++ WebStore/ViewModels/EmployeeView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class EmployeeView
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Profession { get; set; }
+        public int Experience { get; set; }
+
+    }
+}
